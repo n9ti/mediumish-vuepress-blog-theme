f463c464fdc58801db2e1ed5884b6339e3147439
Nati Namvong <nati@flyingcomma.com>
2020-06-15 11:40:40 +0700

    Hide Newsletter and Comment

--- layouts/Post.vue
+++ layouts/Post.vue
@@ -44,8 +44,8 @@ itemtype="https://schema.org/BlogPosting"
 
 <div class="row justify-content-center">
   <div class="col-md-9">
-    <Newsletter v-if="$service.email.enabled" />
-    <Comment />
+    <!-- <Newsletter v-if="$service.email.enabled" />
+    <Comment /> -->
   </div>
 </div>
 
